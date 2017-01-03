628b3b4b4fbc99ba9d4d606f535bab5413737800
zhouzg <304118469@qq.com>
2017-01-03 15:12:57 +0800

    init project

---
+++ src/main/java/cn/ecomb/EcombApplication.java
@@ -0,0 +1,13 @@
+package cn.ecomb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcombApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcombApplication.class, args);
+		System.out.println("test");
+	}
+}
